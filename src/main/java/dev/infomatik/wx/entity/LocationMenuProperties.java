106bca114318afe82c762b8b7919815ce68713b8
thorn <thorn@thorn-B760I-AORUS-PRO-DDR4>
2025-08-03 13:23:02 -0400

    misc and read list from file

---
+++ src/main/java/dev/infomatik/wx/entity/LocationMenuProperties.java
@@ -0,0 +1,51 @@
+package dev.infomatik.wx.entity;
+
+public class LocationMenuProperties {
+
+	
+	
+	private double latitude;   // ":39.338255,
+	private double longitude;   // ":-82.983737,
+	private String locName;
+	/**
+	 * @return the latitude
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	/**
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	/**
+	 * @return the locName
+	 */
+	public String getLocName() {
+		return locName;
+	}
+	/**
+	 * @param locName the locName to set
+	 */
+	public void setLocName(String locName) {
+		this.locName = locName;
+	} 
+	
+	
+	
+	
+	
+}
